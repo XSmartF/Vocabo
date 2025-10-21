db82fbddc34d6c535f073839ebcaa0e75b236a8e
Nhơn Trần <nhonvn2509@gmail.com>
2025-10-21 08:13:26 +0700

    setup page

--- src/shared/layouts/admin.tsx
+++ src/shared/layouts/admin.tsx
@@ -1,7 +1,55 @@
 import { type FC } from 'react';
+import { Outlet } from 'react-router-dom';
+
+import { AppSidebar } from '../components/app-sidebar';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '../components/ui/breadcrumb';
+import { Separator } from '../components/ui/separator';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '../components/ui/sidebar';
 
 const AdminLayout: FC = () => {
-  return <div>AdminLayout</div>;
+  return (
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': '350px',
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar />
+      <SidebarInset>
+        <header className="bg-background sticky top-0 flex shrink-0 items-center gap-2 border-b p-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">All Inboxes</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Inbox</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+
+        <main className="flex flex-1 flex-col gap-4 p-4">
+          <Outlet />
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 };
 
 export default AdminLayout;
